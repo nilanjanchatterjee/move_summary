8dc2c7d9b9d1f2bf0d133b453391f28ad93d89c6
Nilanjan Chatterjee <nilanjanchatterjee7@outlook.com>
2022-06-27 15:10:55 -0500

    Initial commit of files for the movement summary app

---
+++ rfunction.R
@@ -0,0 +1,33 @@
+library(amt)
+library(ggplot2)
+library(move)
+
+
+rFunction <-function(data){
+  data_df <- as.data.frame(data)
+  tr2 <-data_df %>% make_track(., .x=location_long, .y=location_lat, .t=timestamp, 
+                               crs=sp::CRS("+init=epsg:4326"), 
+                               Individual_id=tag_local_identifier)
+  
+  summary<-tr2 %>% nest(data= -"Individual_id") %>%  
+    mutate(sr = map(data, summarize_sampling_rate)) %>% 
+    dplyr::select(Individual_id , sr) %>% 
+    unnest(cols=sr) 
+  #%>% print(n=nrow(tr2))
+  
+  write.csv(summary[,-c(3,6)], file= "Summary_output.csv")
+#write.csv(summary[,-c(3,6)], file= paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"Summary_output.csv"))
+  
+#### plotting the time individuals were radio collared
+  plot.new()
+  pdf( "Time_summary.pdf")
+  #pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "Time_summary.pdf"))
+  summary_plot <-ggplot(data_df) +
+    geom_point(aes(x = timestamp, y = as.factor(tag_local_identifier))) +
+    labs(x= "Time", y= "Individual_id")+
+    theme(legend.position = "none")+
+    theme_bw()
+  
+  print(summary_plot)
+  dev.off()
+}
