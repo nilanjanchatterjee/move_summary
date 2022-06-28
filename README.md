f3e9810cc167b1b992ff7ad97445cada446f8c86
Nilanjan Chatterjee <nilanjanchatterjee7@outlook.com>
2022-06-28 11:09:11 -0500

    Updated readme and rfunction for the repo

--- README.md
+++ README.md
@@ -4,14 +4,18 @@ Moveapps
 Github repository: https://github.com/nilanjanchatterjee/move_summary
 
 ## Description
+The app generates an summary for fix-intervals, number of relocations for each individual_id and a plot for the time interval the collar was active for each individuals in a dataset.
 
 ## Documentation
+
+The output consists of two artefacts, (i) a .csv file with the basic summary statistics (e.g. minimum, maximum, mean and median) of fix intervals and number of relocations for each individual; (ii) a .pdf file with the figure with the time radio-collar was active on each individual.
 
 ## Input data
 
 *move/moveStack* in Movebank format 
 
 ## Output data
+
 ### Artefacts
 - Time_summary.pdf
 - Summary_output.csv
--- rfunction.R
+++ rfunction.R
@@ -5,6 +5,8 @@ library(move)
 
 rFunction <-function(data){
   data_df <- as.data.frame(data)
+  
+### Create track for each individual using function from *amt* package
   tr2 <-data_df %>% make_track(., .x=location_long, .y=location_lat, .t=timestamp, 
                                crs=sp::CRS("+init=epsg:4326"), 
                                Individual_id=tag_local_identifier)
@@ -13,15 +15,17 @@ rFunction <-function(data){
     mutate(sr = map(data, summarize_sampling_rate)) %>% 
     dplyr::select(Individual_id , sr) %>% 
     unnest(cols=sr) 
-  #%>% print(n=nrow(tr2))
   
-  write.csv(summary[,-c(3,6)], file= "Summary_output.csv")
-#write.csv(summary[,-c(3,6)], file= paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"Summary_output.csv"))
+##I am dropping the first and third quartile of the fix interval here from the output, 
+###they can also be included if required
+  
+#write.csv(summary[,-c(3,6)], file= "Summary_output.csv")
+write.csv(summary[,-c(3,6)], file= paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"Summary_output.csv"))
   
 #### plotting the time individuals were radio collared
   plot.new()
-  pdf( "Time_summary.pdf")
-  #pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "Time_summary.pdf"))
+  #pdf( "Time_summary.pdf")
+  pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "Time_summary.pdf"))
   summary_plot <-ggplot(data_df) +
     geom_point(aes(x = timestamp, y = as.factor(tag_local_identifier))) +
     labs(x= "Time", y= "Individual_id")+
